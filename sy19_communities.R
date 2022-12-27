f2e136a57fd32a5f7c2c394f182ce44cf02de76b
DamienVaurs <damien.v560@gmail.com>
2022-12-27 16:15:41 +0100

    Ajout de divers jeux de données avec plusieurs méthodes de remplacement des données manquantes

--- sy19_communities.R
+++ sy19_communities.R
@@ -4,37 +4,75 @@
 # install.packages("ggplot2")
 library(visdat)
 library(ggplot2)
-
-
-data <- read.csv("communities_train.csv")
-
-na_columns <- sapply(data, is.na)
-na_counts <- colSums(na_columns)
-selected_columns <- na_counts > 0
-subset_data <- subset(data, select = selected_columns)
-
-vis_miss(subset_data,warn_large_data = FALSE,sort_miss = TRUE) + theme(text = element_text(size=10),axis.text.x = element_text(angle=90,hjust=0))
+library(randomForest)
+library(missForest)
+library(mice)
+library(missingno)
 
 # Missing values article --> https://journalofbigdata.springeropen.com/articles/10.1186/s40537-021-00516-9
 # Function missingno
 
-################### Données manquantes par régression linéaire #####################
 
-non_numeric_cols <- which(sapply(data, function(x) !is.numeric(x)))
-data_without_non_numeric <- subset(data, select = -non_numeric_cols)
-cols_with_missing_values <- which(sapply(data_without_non_numeric, function(x) any(is.na(x))))
+data <- read.csv("communities_train.csv")
+
+# Jeu de données sans les variables non prédictives (utilisé pour la suite)
+data_without_not_predictive <- data[, -(1:5)]
+
+# Jeu de données dont les données manquantes ont été estimées à l'aide d'une forêt aléatoire
+data_imputed <- missForest(data_without_not_predictive)$ximp 
+
+# Jeu de données sans les observations incomplètes
+data_complete <- data[complete.cases(data), ]
+
+# Jeu de données sans les variables comprenant des observations incomplètes
+data_complete2 <- data[, colSums(is.na(data)) == 0]
+
+# Données manquantes estimées grâce à MICE (ne fonctionne pas)
+data_mice2 <- mice(data_without_not_predictive)
+data_mice <- data_mice2$data
+
+# Données manquantes remplacées par la moyenne des autres données
+data_mean <- data_without_not_predictive
+column_means <- sapply(data_mean, mean, na.rm = TRUE)
+for (col in colnames(data_mean)) {
+  data_mean[, col] <- ifelse(is.na(data_mean[, col]), column_means[col], data_mean[, col])
+}
+
+# Données manquantes remplacées par la médiane des autres données
+data_med <- data_without_not_predictive
+column_meds <- sapply(data_med, median, na.rm = TRUE)
+for (col in colnames(data_med)) {
+  data_med[, col] <- ifelse(is.na(data_med[, col]), column_meds[col], data_med[, col])
+}
+
+
+################### Visualisation des données manquantes #####################
+
+na_columns <- sapply(data_without_not_predictive, is.na)
+na_counts <- colSums(na_columns)
+selected_columns <- na_counts > 0
+subset_data <- subset(data_without_not_predictive, select = selected_columns)
+
+vis_miss(subset_data,warn_large_data = FALSE,sort_miss = TRUE) + theme(text = element_text(size=10),axis.text.x = element_text(angle=90,hjust=0))
+
+
+
+################### Données manquantes par régression linéaire #####################
+# (ne fonctionne pas) #
+
+cols_with_missing_values <- which(sapply(data_without_not_predictive, function(x) any(is.na(x))))
 
 for (col in cols_with_missing_values) {
-  not_missing <- !is.na(data_without_non_numeric[, col])
-  x <- data_without_non_numeric[not_missing, -col]
-  y <- data_without_non_numeric[not_missing, col]
+  not_missing <- !is.na(data_without_not_predictive[, col])
+  x <- data_without_not_predictive[not_missing, -col]
+  y <- data_without_not_predictive[not_missing, col]
   model <- lm(y ~ ., data = x)
 }
 
-missing <- is.na(data_without_non_numeric[, col])
-z <- data_without_non_numeric[missing, -col]
+missing <- is.na(data_without_not_predictive[, col])
+z <- data_without_not_predictive[missing, -col]
 options(warn=-1)
 predictions <- predict(model, newdata = z)
-data_without_non_numeric[missing, col] <- predictions
+data_without_not_predictive[missing, col] <- predictions
 
 
