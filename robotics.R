59f55b1b872bcf27a1f7c0e59c6e561ccfa45ea7
Matthieu Ndumbi Lukuenya <matthieu.ndumbi-lukuenya@etu.utc.fr>
2023-01-07 03:17:07 +0000

    Update robotics.R

--- robotics.R
+++ robotics.R
@@ -1,14 +1,13 @@
-rm(list = ls())
-
 data <- read.table('robotics_train.txt')
 
+# data processing and partitioning. 80% for the train set and the rest for the test set
 index <- sample(nrow(data), 0.8*nrow(data))
 train <- data[index,]
 test <- data[-index,]
 
 # model SVR
 library(caret)
-
+# hyperparameters grid for cost c and sigma values
 c_vals <- c(0.1,0.25,0.5, 1, 10)
 sigma_vals <- c(0.01, 0.1, 1, 10, 100)
 
@@ -32,7 +31,6 @@ print(MSE)
 
 # Optimal values of hyperparameters C and sigma
 best_params <- svr.model$bestTune
-
 best_c <- best_params$C
 #  10
 best_sigma <- best_params$sigma
