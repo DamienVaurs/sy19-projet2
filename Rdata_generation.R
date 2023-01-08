7962104b7f873fe5c562c013695167fae495f0fd
DamienVaurs <damien.v560@gmail.com>
2023-01-08 18:59:20 +0100

    Ajout du rapport communities et optimisation des modèles

--- Rdata_generation.R
+++ Rdata_generation.R
@@ -1,6 +1,22 @@
+library(caret)
+set.seed(5)
+tuneGrid <- expand.grid(
+  C = c(0.25, 0.5, 1),
+  sigma = 0.1
+)
+ctrl <- trainControl(method = "cv", number = 10)
+
+svr.model <- train(y~., data=read.table('robotics_train.txt'), method="svmRadial", trcotrol=ctrl, tuneGrid = tuneGrid)
+
+svr.model.pred <- predict(svr.model, test)
+error <- svr.model.pred - test[["y"]]
+MSE <- mean(error^2)
+print(MSE)
+
+
 # 1. Apprentissage des modèles.
 model.phoneme <- model.phoneme
-model.robotics <- ...
+model.robotics <- svr.model
 model.communities <- model_final
 # 2. Création des fonctions de prédiction
 prediction_phoneme <- function(dataset) {
@@ -11,10 +27,11 @@ prediction_phoneme <- function(dataset) {
   predict(model.phoneme,newdata=testset,type="response")
 }
 prediction_robotics <- function(dataset) {
-  ...
+  prediction_robotics <- predict(model.robotics, newdata = dataset)
 }
 prediction_communities <- function(dataset) {
-  prediction_communities <- predict(model, newdata = dataset)
+  library(randomForest)
+  prediction_communities <- predict(model.communities, newdata = dataset)
 }
 # 3. Sauvegarder sous forme de fichier .Rdata les fonctions
 # ’prediction_phoneme’, ’prediction_robotics’, ’prediction_communities’.
--- sy19_communities.R
+++ sy19_communities.R
@@ -16,6 +16,8 @@ library(MASS)
 library(glmnet)
 library(leaps)
 library(rpart)
+library(corrplot)
+library(graphics)
 set.seed(123)
 
 # Missing values article --> https://journalofbigdata.springeropen.com/articles/10.1186/s40537-021-00516-9
@@ -77,6 +79,9 @@ for (col in colnames(data_med)) {
 }
 
 
+
+
+
 ################### Visualisation des données manquantes #####################
 
 na_columns <- sapply(data_without_not_predictive, is.na)
@@ -84,7 +89,9 @@ na_counts <- colSums(na_columns)
 selected_columns <- na_counts > 0
 subset_data <- subset(data_without_not_predictive, select = selected_columns)
 
-vis_miss(subset_data,warn_large_data = FALSE,sort_miss = TRUE) + theme(text = element_text(size=10),axis.text.x = element_text(angle=90,hjust=0))
+vis_miss(subset_data,warn_large_data = FALSE,sort_miss = TRUE) + theme(text = element_text(size=10),axis.text.x = element_text(angle=90,hjust=0)) +
+                                                                         labs(title="Diagramme des données manquantes", x="Prédicteurs", y="Observations") +
+  theme(plot.title = element_text(hjust = 0.5))
 
 
 
@@ -114,6 +121,64 @@ data_without_not_predictive[missing, col] <- predictions
 
 ################### Modèles de prédiction #####################
 
+make_one_formula <- function(names) {
+  # Création de la formule correspondante au modèle i
+  f <- "y ~"
+  for (name in names) {
+    f <- paste(f, "+", name)
+  }
+  return (f)
+}
+
+
+make_formula <- function(reg.fit) {
+  
+  Formula <- c()
+  for (i in 2:length(summary(reg.fit)$which[1,])-1) {
+    
+    model <- summary(reg.fit)$which[i,] # Masque du modèle i
+    model <- model[2:length(model)] # On ne prend pas la colone (Intercept)
+    model.names <- names(model)[model]
+    
+    names <- model.names[1:length(model.names)]
+    f <- make_one_formula(names)
+    
+    # Ajout de la formule à la liste des formules
+    Formula <- append(Formula, f)
+  }
+  
+  return (Formula)
+}
+
+
+n1 <- nrow(data_train)
+K1 <- 5
+K2 <- 10
+folds1 <- sample(1:K1, n1, replace=TRUE)
+CV1 <- rep(0, length(Formula))
+for (k1 in (1:K1)){
+  inner.data <- data_train[folds1 != k1, ]
+  n2 <- nrow(inner.data)
+  folds2 <- sample(1:K2, n2, replace=TRUE)
+  CV2 <- rep(0, length(Formula))
+  for(i in (1:length(Formula))) {
+    for(k2 in (1:K2)) {
+      reg <- lm(Formula[i], data=inner.data[folds2 != k2, ])
+      pred <- predict(reg, newdata=inner.data[folds2 == k2, ])
+      CV2[i] <- CV2[i] + sum((inner.data$y[folds2 == k2] - pred)^2)
+    }
+    CV2[i] <- CV2[i]/n2
+  }
+  CV1 <- CV1 + CV2
+}
+CV1 <- CV1/K1
+
+par(mfrow = c(1, 2))
+sol <- which.min(CV1)
+plot(CV1, type="b")
+
+
+
 
 split <- createDataPartition(y = data_complete2$ViolentCrimesPerPop, p = 0.9, list = FALSE)
 data_train <- data_complete2[split, ]
@@ -138,6 +203,9 @@ fit <- regsubsets(ViolentCrimesPerPop ~ ., data = data_train, really.big = T)
 summary(fit)
 plot(fit, scale = "Cp")
 
+lm.reg.fit <- regsubsets(ViolentCrimesPerPop~., data=data_train, method='backward', nvmax=length(data_train)-1)
+Formula <- make_formula(lm.reg.fit)
+
 
 ################### Prédiction avec Mclust #####################
 
@@ -149,12 +217,14 @@ plot(fit, scale = "Cp")
 model <- lm(ViolentCrimesPerPop ~ ., data = data_train)
 summary(model)
 predictions <- predict(model, newdata = data_test)
-mse <- mean((predictions - data_test$ViolentCrimesPerPop)^2)
-print(mse)
-liste_mse <- c(liste_mse, list(RegLinéaire = mse))
-tab_mse <- rbind(modele = "RegLinéaire", mse = mse)
+mse3 <- mean((predictions - data_test$ViolentCrimesPerPop)^2)
+print(mse3)
 
-results <- train(ViolentCrimesPerPop ~ ., data = data_complete2, method = "lm", trControl = cv, metric = metric)
+results <- train(ViolentCrimesPerPop ~ ., data = data_train, method = "lm", trControl = cv, metric = metric)
+predictions <- predict(model, newdata = data_test)
+mse4 <- mean((predictions - data_test$ViolentCrimesPerPop)^2)
+print(mse4)
+liste_mse <- c(liste_mse, list(RegLinéaire = min(mse3, mse4)))
 print(results)
 
 ################### Prédiction avec forêt aléatoire #####################
@@ -162,17 +232,19 @@ print(results)
 model <- randomForest(ViolentCrimesPerPop ~ ., data = data_train)
 summary(model)
 predictions <- predict(model, newdata = data_test)
-mse <- mean((predictions - data_test$ViolentCrimesPerPop)^2)
-print(mse)
-liste_mse <- c(liste_mse, list(RandomForest = mse))
-tab_mse <- rbind(modele = "RandomForest", mse = mse)
+mse1 <- mean((predictions - data_test$ViolentCrimesPerPop)^2)
+print(mse1)
+
+
 
 model <- train(ViolentCrimesPerPop ~ ., data = data_train, method = "rf", trControl = trainControl(method = "cv", number = 10))
+print(model)
 predictions <- predict(model, newdata = data_test)
-mse <- mean((predictions - data_test$ViolentCrimesPerPop)^2)
-print(mse)
-liste_mse <- c(liste_mse, list(RandomForestCV = mse))
-tab_mse <- rbind(modele = "RandomForestCV", mse = mse)
+mse2 <- mean((predictions - data_test$ViolentCrimesPerPop)^2)
+print(mse2)
+liste_mse <- c(liste_mse, list(RandomForestCV = min(mse1, mse2)))
+
+
 
 ################### Prédiction avec réseau de neurones #####################
 
@@ -205,31 +277,38 @@ liste_mse <- c(liste_mse, list(GAM = mse))
 
 model <- glmnet(x = X_train, y = y_train, alpha = 1)
 predictions <- predict(model, newx = as.matrix(X_test))
-mse <- mean((predictions - data_test$ViolentCrimesPerPop)^2)
-print(mse)
-liste_mse <- c(liste_mse, list(Lasso1 = mse))
+mse5 <- mean((predictions - data_test$ViolentCrimesPerPop)^2)
+print(mse5)
 
 cv.out <- cv.glmnet(as.matrix(X_train), y_train, alpha=1, standardize=TRUE)
 model <- glmnet(X_train, y_train, lambda=cv.out$lambda.min, alpha=1, standardize=TRUE)
 predictions <- predict(model, s=cv.out$lambda.min, newx = as.matrix(X_test))
-mse <- mean((predictions - data_test$ViolentCrimesPerPop)^2)
-print(mse)
-liste_mse <- c(liste_mse, list(Lasso2 = mse))
+mse6 <- mean((predictions - data_test$ViolentCrimesPerPop)^2)
+print(mse6)
+liste_mse <- c(liste_mse, list(Lasso = min(mse5, mse6)))
 
 ################### Prédiction avec Regression Ridge #####################
 
 model <- glmnet(as.matrix(X_train), y_train, alpha = 0, lambda = 0.1)
 predictions <- predict(model, newx = as.matrix(X_test))
-mse <- mean((predictions - data_test$ViolentCrimesPerPop)^2)
-print(mse)
-liste_mse <- c(liste_mse, list(Ridge1 = mse))
+mse7 <- mean((predictions - data_test$ViolentCrimesPerPop)^2)
+print(mse7)
 
 cv.out <- cv.glmnet(as.matrix(X_train), y_train, alpha=0, standardize=TRUE)
 model <- glmnet(X_train, y_train, lambda=cv.out$lambda.min, alpha=0, standardize=TRUE)
 predictions <- predict(model, s=cv.out$lambda.min, newx = as.matrix(X_test))
-mse <- mean((predictions - data_test$ViolentCrimesPerPop)^2)
-print(mse)
-liste_mse <- c(liste_mse, list(Ridge2 = mse))
+mse8 <- mean((predictions - data_test$ViolentCrimesPerPop)^2)
+print(mse8)
+
+cv.out <- cv.glmnet(as.matrix(X_train), y_train, alpha=0)
+plot(cv.out)
+cv.out$lambda.min
+model <- glmnet(X_train, y_train, lambda=cv.out$lambda.min, alpha=0)
+predictions <- predict(model, s=cv.out$lambda.min, newx=as.matrix(X_test))
+mse9 <- mean((predictions - data_test$ViolentCrimesPerPop)^2)
+print(mse9)
+liste_mse <- c(liste_mse, list(Ridge = min(mse7,mse8,mse9)))
+
 
 ################### Prédiction avec Regression Ridge et critère BIC #####################
 
@@ -246,6 +325,17 @@ predictions <- predict(model, newx = as.matrix(X_test))
 mse <- mean((predictions - data_test$ViolentCrimesPerPop)^2)
 print(mse)
 liste_mse <- c(liste_mse, list(LassoBIC = mse))
+
+################### Prédiction avec Regression elastic net #####################
+
+cv.out <- cv.glmnet(as.matrix(X_train), y_train, alpha=0.5)
+plot(cv.out)
+cv.out$lambda.min
+model <- glmnet(X_train, y_train, lambda=cv.out$lambda.min, alpha=0.5)
+predictions <- predict(model, s=cv.out$lambda.min, newx=as.matrix(X_test))
+mse <- mean((predictions - data_test$ViolentCrimesPerPop)^2)
+print(mse)
+liste_mse <- c(liste_mse, list(ElasticNet = mse))
 
 ################### Prédiction avec arbre de régression #####################
 
@@ -276,19 +366,44 @@ ypred<-X[,res.forward$which[best,]]%*%coef(model,best)
 mse <-mean((ypred-data_test$ViolentCrimesPerPop)^2)
 liste_mse <- c(liste_mse, list(SubSelVar = mse))
 
+lm.reg.fit.summary <- summary(lm.reg.fit)
+bic <- lm.reg.fit.summary$outmat[which.min(lm.reg.fit.summary$bic), ]
+bic.predicteurs <- names(data_train)[bic == "*"]
+bic.predicteurs <- bic.predicteurs[-length(bic.predicteurs)] # Attention  enlever le y de la formule lorsque le backward est utilis
+plot(lm.reg.fit, scale="bic")  # 46 predicteurs
+f <- make_one_formula(bic.predicteurs)
+model <- lm(f, data_train)
+predictions <- predict(model, newdata=data_test)
+mse <- mean((predictions - data_test$ViolentCrimesPerPop)^2)
+print(mse)
+
+################### Prédiction avec SVM #####################
+
+model <- svm(ViolentCrimesPerPop ~ ., data = data_train, type = "eps-regression")
+predictions <- predict(model, newdata = data_test)
+mse10 <- mean((predictions - data_test$ViolentCrimesPerPop)^2)
+print(mse10)
+
+model <- train(ViolentCrimesPerPop ~ ., data = data_train, method = "svmRadial", trControl = trainControl(method = "cv", number = 10))
+print(model)
+predictions <- predict(model, newdata = data_test)
+mse11 <- mean((predictions - data_test$ViolentCrimesPerPop)^2)
+print(mse11)
+liste_mse <- c(liste_mse, list(SVM = min(mse10, mse11)))
 
 
-model_final <- randomForest(ViolentCrimesPerPop ~ ., data = data_complete2)
-summary(model)
-
-keys <- names(liste_mse)
-values <- unname(liste_mse)
-plot(t(keys), values, type="b", xlab="Clés", ylab="Valeurs")
-plot(unlist(liste_mse))
+model_final <- train(ViolentCrimesPerPop ~ ., data = data_complete2, method = "rf", trControl = trainControl(method = "cv", number = 10))
+print(model_final)
+summary(model_final)
 
 
-model_names
-ggplot(df, aes(x=model_names, y=mse_values)) + geom_bar(stat="identity")
+
+df <- stack(liste_mse)
+df$Legende <- ifelse(df$values == min(df$values), "lowest", "not_lowest")
+ggplot(df, aes(x = ind, y = values, , fill=Legende)) + geom_bar(stat = "identity")+
+  labs(title="Diagramme en barres des MSE de chaque modèle étudié", x="Modèle", y="MSE") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  scale_fill_manual(values = c("lowest" = "red", "not_lowest" = "blue"), labels = c("Valeur la plus basse", "Autres valeurs"))
 
 
 
